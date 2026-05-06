43b0fa4b36d5833e91a282c60b08a46c77ad9dee
Felicia Nordgren <fenor@kth.se>
2026-05-06 16:26:26 +0200

    Test: kan läsa enskild pixel

--- src/main.go
+++ src/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/jpeg"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("../img/test.jpg")
+	file, err := os.Open("../img/test2.jpg")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Hittade inte bilden: ", err)
 		return
 	}
 	defer file.Close()
-	fmt.Println("Opened")
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("Kunde inte läsa bilden: ", err)
+		return
+	}
+
+	pixel := img.At(50, 120)
+	r, g, b, _ := pixel.RGBA()
+
+	fmt.Printf("Röd: %d, Grön: %d, Blå: %d\n", r>>8, g>>8, b>>8)
+
 }
