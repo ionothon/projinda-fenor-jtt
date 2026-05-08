08f432f8a4c5f562e544a6bd47f10e2de32c31cf
Felicia Nordgren <fenor@kth.se>
2026-05-08 16:38:06 +0200

    Test: can determine dominant color

--- src/main.go
+++ src/main.go
@@ -21,9 +21,28 @@ func main() {
 		return
 	}
 
-	pixel := img.At(50, 120)
-	r, g, b, _ := pixel.RGBA()
+	redBucket := 0
+	greenBucket := 0
+	blueBucket := 0
 
-	fmt.Printf("Röd: %d, Grön: %d, Blå: %d\n", r>>8, g>>8, b>>8)
+	bounds := img.Bounds()
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			R := r >> 8
+			G := g >> 8
+			B := b >> 8
+
+			if R > G && R > B {
+				redBucket++
+			} else if G > R && G > B {
+				greenBucket++
+			} else if B > R && B > G {
+				blueBucket++
+			}
+		}
+	}
+	fmt.Printf("Resultat: Röd - %d, Grön - %d, Blå - %d", redBucket, greenBucket, blueBucket)
 
 }
